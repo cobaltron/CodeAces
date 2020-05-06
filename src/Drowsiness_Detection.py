f904937430e47c24e017afa1c26bb36421ade3c6
cobaltron <rlahiri1008@outlook.com>
2020-05-06 19:11:54 +0530

    Added Main Manager

--- src/Drowsiness_Detection.py
+++ src/Drowsiness_Detection.py
@@ -136,51 +136,65 @@ class BuzzerAPI:
 
 class MainManager:
 
-    global ps  #global flag variable used to keep playing the buzzer
+    global ps #global flag variable used to keep playing the buzzer
     ps=True
     EYE_AR_CONSEC_FRAMES = 25
-    COUNTER=0
+    YAWN_CONSEC_FRAMES = 5
+    EYE_COUNTER=0
+    YAWN_COUNTER=0
     ALARM_ON=False
     def main(self):
-        video_capture = cv2.VideoCapture(0) #Video is being captured from webcam
+        video_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW) #Video is being captured from webcam
         c=0
         while(True):
-            ret, frame = video_capture.read() #Read frame if present and return value for ret
+            ret, frame = video_capture.read() #Video is being captured from webcam
             f=Frame(frame)
-            cv2.imshow("out",f.res()) #Display the frame
-            if(cv2.waitKey(1) & 0xFF == ord('q')):#Option for termination of display window
+            cv2.imshow("out",f.res())
+            if(cv2.waitKey(1) & 0xFF == ord('q')):
                 break
-
-            #Objects of classes used for calling methods    
+            #Objects of classes used for calling methods
             fm=FaceDetectionManager()
             em=EyeDetectionManager()
-            ecm=EyeClosureManager()      
+            ecm=EyeClosureManager()
+            mm=MouthDetectionManager()
+            ym=YawningManager() 
             ba=BuzzerAPI()
 
             ret,land=fm.getFace(f.res()) #Read landmark coordinates if present and return value for ret
-            leye,reye=em.getEye(land)   #Landmark coordibates for left and right eye
-            if(ecm.Drowsiness_Detected(leye,reye)==1): #Check whether drowsy or not
-                self.COUNTER += 1
-                if self.COUNTER >= self.EYE_AR_CONSEC_FRAMES:
+            leye,reye=em.getEye(land) #Landmark coordibates for left and right eye
+            upper,lower=mm.getMouth(land) #landmark coordinates for mouth
+            if(ecm.Drowsiness_Detected(leye,reye)==1):
+                self.EYE_COUNTER += 1
+                if self.EYE_COUNTER >= self.EYE_AR_CONSEC_FRAMES:
                     ps=True
                     if not self.ALARM_ON:
                         self.ALARM_ON = True
-                        t = Thread(target=ba.alarm) #A deamon thread is created for activating the alarm if drowsiness is detected
+                        t = Thread(target=ba.alarm)#A deamon thread is created for activating the alarm if drowsiness is detected
                         t.deamon = True
                         t.start()
             elif(ecm.Drowsiness_Detected(leye,reye)==-1):
-                c=c+1
-                if(c%45==0): #To prevent overlapping buzzer playing(due to creatiion of multiple threads) using a set number of frames found using trial and error
-                    t1 = Thread(name='Thread-a',target=ba.alert) #A deamon thread is created for activating the alert if no face or more than one face is detected
-                    t1.setDaemon(True)
-                    t1.start()
+                t1 = Thread(name='Thread-a',target=ba.alert)#A deamon thread is created for activating the alarm if drowsiness is detected
+                t1.setDaemon(True)
+                t1.start()
             else:
-                self.COUNTER = 0
+                self.EYE_COUNTER = 0
                 self.ALARM_ON = False
                 ps=False    
-                
+
+            if(ym.Drowsiness_Detected(upper,lower)==1):
+                self.YAWN_COUNTER += 1
+                if self.YAWN_COUNTER >= self.YAWN_CONSEC_FRAMES:
+                    ps=True
+                    if not self.ALARM_ON:
+                        self.ALARM_ON = True
+                        t = Thread(target=ba.alarm)#A deamon thread is created for activating the alarm if drowsiness is detected
+                        t.deamon = True
+                        t.start()
+            else:
+                self.YAWN_COUNTER = 0
+                self.ALARM_ON = False
+                ps=False 
         video_capture.release()
         cv2.destroyAllWindows()
-
 ob=MainManager() # main manager object is being created
 ob.main() #method for main manager is being called
